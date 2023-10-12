9954623875d99a2aabe264b99efd613bbf6caf33
JoshuaToback <joshuatoback@gmail.com>
2023-10-12 15:20:07 -0400

    Added updated about section

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,12 +1,11 @@
-'use client'
 import React from "react";
-
+import { AiFillTwitterCircle } from "react-icons/ai"; // Import the Twitter icon
 import PageTransition from "@/components/PageTransition";
 import Josh from "@/images/headshot.png";
-import Andrea from "@/images/andreareel.png";
+import Andrea from "@/images/Andrea2023.png";
 import Violet from "@/images/violet.png";
 import ghoul from "@/images/flgl.png";
-import Chris from "@/images/falcon.png";
+import Chris from "@/images/chris.jpg";
 import RMM from "@/images/RMM.png";
 import Image from "next/image";
 
@@ -17,6 +16,7 @@ const About = () => {
       title: "Project Lead and Casting Director",
       image: Josh,
       bio: "The founder of Open Mic Studios, Joshua began voice acting in 2007 when he recorded voice over for his LEGO stopmotion animations. Little did he know that the voice acting aspect would turn into his greatest passion. He is now a casting director aspiring to create a bridge for everyone to cross over into the voiceover industry!",
+      twitterUsername: "JTVoiceActor",
     },
 
     {
@@ -24,6 +24,7 @@ const About = () => {
       title: "Casting Director",
       image: RMM,
       bio: "Gordon is a Voice Actor, Casting director and Content Creator based in Houston TX.  With over 10+ years of production experience ranging from television, to advertising, to Youtube content creation, Gordon has the experience to help make your project a reality!",
+      twitterUsername: "RedMufflerMan",
     },
 
     {
@@ -31,6 +32,7 @@ const About = () => {
       title: "Casting Director and Voice Coach",
       image: Andrea,
       bio: "Andrea is a classically trained singer and actor with a passion for performance! She’s been voice acting for 3 years but has been an actor since childhood. She loves to direct and bring people into the voice over community. She is always around and willing to help!",
+      twitterUsername: "dreyahVA",
     },
 
     {
@@ -45,6 +47,7 @@ const About = () => {
       title: "Casting Director and Designer",
       image: ghoul,
       bio: "Luis is a longtime artist and voice actor who's a voice actor for fun and an artist for real! His passion for art of both digital and voice will paint a new picture for you!",
+      twitterUsername: "gH0uLVA",
     },
 
     {
@@ -52,6 +55,7 @@ const About = () => {
       title: "Casting Director",
       image: Chris,
       bio: "Starting his voice acting journey in 2014, Christiaan has kept chasing the dream to this day. He has joined OMS to help others chase said dream!",
+      twitterUsername: "Cptfalcon54",
     },
   ];
 
@@ -61,17 +65,28 @@ const About = () => {
         <h1 className="text-3xl font-semibold mb-6">About Us</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {teamMembers.map((member, index) => (
-            <div key={index} className="bg-slate-300 rounded-lg shadow-md p-6">
+            <div
+              key={index}
+              className="bg-slate-300 rounded-lg shadow-md p-6 flex flex-col items-center"
+            >
               <Image
                 src={member.image}
                 alt={member.name}
                 width={128}
                 height={128}
-                className="w-32 h-32 rounded-full mx-auto mb-4"
+                className="w-32 h-32 rounded-full mb-4"
               />
               <h2 className="text-2xl font-semibold">{member.name}</h2>
               <h3 className="text-xl text-gray-600 mb-2">{member.title}</h3>
               <p className="text-lg font-base text-gray-700">{member.bio}</p>
+              <a
+                href={`https://twitter.com/${member.twitterUsername}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:text-blue-700 transition duration-300 ease-in-out mt-4"
+              >
+                <AiFillTwitterCircle className="w-10 h-10" />
+              </a>
             </div>
           ))}
         </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-import PageTransition from "@/components/PageTransition";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
@@ -15,69 +14,61 @@ import heroImage from "../public/hero.jpg";
 
 export default function Home() {
   return (
-    <PageTransition>
-      <main className="flex mx-auto flex-col items-center justify-center py-2 min-h-screen">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-        >
-          <section
-            style={{
-              background: `url(${heroImage}) no-repeat center center fixed`,
-              backgroundSize: "cover",
-            }}
-            className="bg-hero-image bg-cover bg-center h-screen w-screen flex flex-col justify-center items-center relative"
-          >
-            <div className="absolute inset-0 bg-black opacity-50"></div>
-            <h1 className="text-white bg-clip-text text-7xl font-light leading-tight  z-10 relative">
-              Open Mic Studios
-            </h1>
-            <p className="text-4xl text-white font-semilight text-center z-10 relative text-shadow">
-              Putting You On
-              <br />
-               Center Stage
-            </p>
+    <main className="flex mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <section
+        style={{
+          background: `url(${heroImage}) no-repeat center center fixed`,
+          backgroundSize: "cover",
+        }}
+        className="bg-hero-image bg-cover bg-center h-screen w-screen flex flex-col justify-center items-center relative"
+      >
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <h1 className="text-white bg-clip-text text-7xl font-light leading-tight  z-10 relative">
+          Open Mic Studios
+        </h1>
+        <p className="text-4xl text-white font-semilight text-center z-10 relative text-shadow">
+          Putting You On
+          <br />
+          Center Stage
+        </p>
 
-            {/* New Buttons */}
-            <div className="mt-6 z-10 relative flex space-x-4">
-              <ScrollLink to="who" spy={true} smooth={true} duration={500}>
-                <button className="bg-slate-500 hover:bg-slate-900 text-white text-3xl font-light py-2 px-4 mx-2 rounded">
-                  About Us
-                </button>
-              </ScrollLink>
-              <ScrollLink to="contact" spy={true} smooth={true} duration={500}>
-                <button className="bg-slate-500 hover:bg-slate-900 text-white text-3xl font-light py-2 px-4 mx-2 rounded">
-                  Contact
-                </button>
-              </ScrollLink>
-            </div>
-            <Image
-              src={heroImage}
-              layout="fill"
-              objectFit="cover"
-              alt="Open Mic Studios"
-              className="filter blur-md" // Add the "filter blur-md" class to apply the blur effect
-            />
-          </section>
-        </motion.div>
+        {/* New Buttons */}
+        <div className="mt-6 z-10 relative flex space-x-4">
+          <ScrollLink to="who" spy={true} smooth={true} duration={500}>
+            <button className="bg-slate-500 hover:bg-slate-900 text-white text-3xl font-light py-2 px-4 mx-2 rounded">
+              About Us
+            </button>
+          </ScrollLink>
+          <ScrollLink to="contact" spy={true} smooth={true} duration={500}>
+            <button className="bg-slate-500 hover:bg-slate-900 text-white text-3xl font-light py-2 px-4 mx-2 rounded">
+              Contact
+            </button>
+          </ScrollLink>
+        </div>
+        <Image
+          src={heroImage}
+          layout="fill"
+          objectFit="cover"
+          alt="Open Mic Studios"
+          className="filter blur-md" // Add the "filter blur-md" class to apply the blur effect
+        />
+      </section>
 
-        <motion.div>
-          <Who />
-        </motion.div>
+      <motion.div>
+        <Who />
+      </motion.div>
 
-        <motion.div>
-          <Element name="services">
-            <Services />
-          </Element>
-        </motion.div>
+      <motion.div>
+        <Element name="services">
+          <Services />
+        </Element>
+      </motion.div>
 
-        <motion.div>
-          <Element name="contact">
-            <Contact />
-          </Element>
-        </motion.div>
-      </main>
-    </PageTransition>
+      <motion.div>
+        <Element name="contact">
+          <Contact />
+        </Element>
+      </motion.div>
+    </main>
   );
 }
